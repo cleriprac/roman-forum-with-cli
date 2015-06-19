6f9d922420a6e6136250fcdf7ae9ace1748d75c8
cleriprac <cleri.phil@gmail.com>
2015-06-19 10:33:39 -0700

    add questions and new-question

---
+++ app/controllers/questions/new.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+   save: function() {
+     var question = this.store.createRecord('question', {
+       question: this.get('question'),
+       description: this.get('description'),
+       author: this.get('author'),
+     });
+
+     question.save();
+
+     this.set('question', '');
+     this.set('description', '');
+     this.set('author', '');
+
+
+   }
+ }
+});
--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('questions', { path: '/'}, function() {
+    this.route('new');
+  });
+  this.resource('question', { path: ':question_id'});
 });
 
 export default Router;
